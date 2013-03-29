2e87bf39793816df0da44787e09e867b0496f497
Tom DeHart <tdehart@gmail.com>
2013-03-29 10:56:59 -0400

    added owfapp portion

--- js/owfapp.js
+++ js/owfapp.js
@@ -1,9 +1,34 @@
 if (OWF.Util.isRunningInOWF()) {
-    // -----------------------------------
-    // Initialize
-    // -----------------------------------
-    OWF.ready(function() {
-        OWF.notifyWidgetReady();
-        console.log("Earthquake Control Panel ready.");
-    });
+	// -----------------------------------
+	// Initialize
+	// -----------------------------------
+	OWF.ready(function() {
+
+		$('#retrieveQuakes').submit(function() {
+			OWF.getOpenedWidgets(function(widgets) {
+				var widget;
+
+				for (var i = 0; i < widgets.length; i++) {
+					if (widgets[i].universalName === 'org.owfgoss.owf.examples.earthquake.data') {
+						widget = widgets[i];
+						break;
+					}
+				}
+
+				if (widget) {
+					OWF.RPC.getWidgetProxy(widget.id, function(proxy) {
+						console.log(proxy);
+						var quakes = proxy.getData();
+						console.log(quakes);
+					});
+				}
+			});
+
+			return false;
+		});
+
+		OWF.notifyWidgetReady();
+
+		console.log("Earthquake Control Panel ready.");
+	});
 }
